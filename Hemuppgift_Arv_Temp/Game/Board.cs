5c9f6807c28dc3f37671f4ead3b64e1dc7c15bc5
JohannaSchroder <johannahell@hotmail.com>
2024-10-11 10:07:22 +0200

    skapat klassen Player

---
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    internal class Board
+    {
+        //Egenskap
+        public int NoPins { get; set; }
+
+        //Konstruktor
+        public Board(int NoPins)
+        { 
+            this.NoPins = NoPins;
+        }
+
+
+        //Metoder
+        public void SetUp()
+        {
+
+        }
+
+        public void TakePins()
+        {
+
+        }
+        public int GetNoPins()
+        { 
+        
+        }
+
+
+    }
+}
--- Hemuppgift_Arv_Temp/Game/Player.cs
+++ Hemuppgift_Arv_Temp/Game/Player.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hemuppgift_Arv_Temp
+namespace Hemuppgift_Arv_Temp.Game
 {
-    internal class Board
+    internal class Player
     {
     }
 }
