7c0d30ccc698b5795810200d6dd5818d40d8d6e5
JohannaSchroder <johannahell@hotmail.com>
2024-10-14 14:00:00 +0200

    lagt till kommentarer

--- Hemuppgift_Arv_Temp/Game/Board.cs
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -13,32 +13,32 @@ namespace Hemuppgift_Arv_Temp.Game
         public int NoPins { get; private set; }
 
         //Metoder
-        public void SetUp(int PinnarAntal)
+        public void SetUp(int PinnarAntal)//Skriver ut hur många pinnar spelet börjar med
         {
             NoPins = PinnarAntal;
             Console.WriteLine($"Det finns {PinnarAntal} på bordet.");
         }
 
-        public bool takePins(int PinnarTagna)//valde en bool
+        public bool takePins(int PinnarTagna)//valde en bool så spelet ska rulla
         {
             if (PinnarTagna < 1 || PinnarTagna > 2)
             {
                 Console.WriteLine("Du kan bara välja 1 eller 2 pinnar i taget.");
-                return false;
+                return false;//man kan bara välja max 2 pinnar
             }            
             else if(PinnarTagna > NoPins)
             {
                 Console.WriteLine("Det finns inte så många pinnar kvar.");
-                return false;
+                return false;//om man väljer fler än vad som finns kvar
             }
             else
             {
-                NoPins -= PinnarTagna;
+                NoPins -= PinnarTagna;//Räknar bort pinnar som datorn eller spelaren väljer
                 return true;
             }
         }
 
-        public void GetNoPins()
+        public void GetNoPins()//hur många pinnar som finns kvar i spelet
         {
             Console.WriteLine($"Det finns {NoPins} kvar.");
         }
--- Hemuppgift_Arv_Temp/Game/TakePins.cs
+++ Hemuppgift_Arv_Temp/Game/TakePins.cs
@@ -12,40 +12,45 @@ namespace Hemuppgift_Arv_Temp.Game
             Board board = new Board();//hämtar Board-klassen 
             board.SetUp(10);//Väljer antal pinnar till spelet
 
+            //Ett litet välkomstmeddelande
             Console.WriteLine("Välkommen till ett mycket enkelt spel!");
             Console.WriteLine($"Du får ta 1-2pinnar");
             Console.WriteLine("Den som tar den sista pinnen förlorar. Lycka till!");
 
+            //så spelet fortsätter tills kravet uppfylls om 1 eller färre pinnar är kvar på bordet
             while (board.NoPins >= 1)
             {
-                bool InteOkejInmatning = false;
+                bool InteOkejInmatning = false;//Kollar så inmatning blir rätt
 
-                while (!InteOkejInmatning)
+                while (!InteOkejInmatning)//så loopen fortsätter tills rätt inmatning gjorts
                 {
                     Console.WriteLine("Hur många pinnar vill du ta?");
                     int PinnarUser = Convert.ToInt32(Console.ReadLine());
                     InteOkejInmatning = board.takePins(PinnarUser);
+                    //kontrollerar med takepins metoden så rätt antal pinnar tas och spelet går vidare.
                 }
 
-                board.GetNoPins();
+                board.GetNoPins();//Hur många pinnar som finns kvar
 
                 if(board.NoPins < 1)
                 { 
-                    Console.WriteLine("Spelet är slut! Du förlorade!");
+                    Console.WriteLine("Spelet är slut! Datorn vinner!");
                     break;
+                    //Datorn vinner
                 }
 
                 Random random = new Random();
                 int PinnarDator = random.Next(1, 3);//för att slumpa tal mellan 1-2
 
-                Console.WriteLine($"Datorn har tagit {PinnarDator} antal pinnar.");
+                Console.WriteLine($"Datorn har tagit {PinnarDator} antal pinnar.");//datorn tar pinnar
                 board.takePins(PinnarDator);
-                board.GetNoPins();
+                board.GetNoPins();//hur många pinnar som finns kvar
 
                 if (board.NoPins < 1)
                 {
-                    Console.WriteLine("Spelet är slut! Datorn förlorade!");
+                    Console.WriteLine("Spelet är slut! Du vinner!");
                     break;
+                    //spelaren vinner
                 }
 
             }
