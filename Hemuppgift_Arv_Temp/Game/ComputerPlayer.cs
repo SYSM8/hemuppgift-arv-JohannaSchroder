734a43e02e656369c7ca3880d69f2e3ffe5095f6
JohannaSchroder <johannahell@hotmail.com>
2024-10-14 21:08:31 +0200

    försökt att få till strategin...

--- Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
+++ Hemuppgift_Arv_Temp/Game/ComputerPlayer.cs
@@ -8,7 +8,7 @@ namespace Hemuppgift_Arv_Temp.Game
 {
     internal class ComputerPlayer : Player//ärver från Player
     {
-       
+       Random random = new Random();
         //konstruktor
         public ComputerPlayer(string UserId) : base(UserId)
         {
@@ -21,18 +21,22 @@ namespace Hemuppgift_Arv_Temp.Game
             return UserId;
         }
 
-        //när det är datorns tur vill jag att det ska finnas 4st pinnar kvar.
-        //då kan spelar det ingen roll om Player tar 1 eller 2 pinnar för datorn vinner oavsett.
+
+        //efter datorn valt vill jag att det ska finnas 4st pinnar kvar.
+        //då spelar det ingen roll om Player tar 1 eller 2 pinnar för datorn vinner oavsett.
+        //här har jag försökt att datorn ska dela med 4 och att det ska bli lika med noll
+        //för att den ska fatta att det ska vara 4 kvar till motståndaren.
         public override int TakePins(Board board)
         {
             int PinnarDator;
             if ((board.NoPins % 4) == 0)
             {
-                PinnarDator = new Random().Next(1, 3);//slumpar 1-2 pinnar och deklarerar det i en variabel
+                PinnarDator = random.Next(1, 3);//slumpar 1-2 pinnar och deklarerar det i en variabel
             }
             else
             {
-                PinnarDator = board.NoPins % 4;
+                PinnarDator = board.NoPins % 2;
+                PinnarDator = random.Next(1, 3);
             }
             
             Console.WriteLine($"{UserId} har tagit {PinnarDator}st pinnar");//visar hur många pinnar datorn tagit
